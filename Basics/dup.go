4386d4016a8b688f4aae662d314116d29642cd4c
lesmonsaluta <lesmon_saluta@dlsu.edu.ph>
2023-03-04 01:40:35 +0800

    syntax, loops, types, some default functions

---
+++ Basics/dup.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// key is the string, the value is the int
+	counts := make(map[string]int)
+	
+	// defining how input works
+	input := bufio.NewScanner(os.Stdin)
+	
+	// while there is an input (input.Scan() returns True), the value 
+	// of input.Text() (that specific line that we are counting)
+	// should be increased
+	
+	// the string is the key, adding another same string increases the value by 1
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	// range iterates over every element in counts
+	// counts is (String (key), Int (value)), hence 
+	// line is the key, n is the value
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
--- Basics/hello.go
+++ Basics/hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, Angkas!")
+	fmt.Println(os.Args[1:])
+	s, sep := "", ""
+	for index, arg := range os.Args[0:] {
+		fmt.Println(index, arg)
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
 }
