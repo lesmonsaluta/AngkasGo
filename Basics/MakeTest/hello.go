6336639687aed67b05585dccb28c2e9db887bfa0
lesmonsaluta <lesmon_saluta@dlsu.edu.ph>
2023-03-09 01:18:55 +0800

    maps

--- Basics/MakeTest/hello.go
+++ Basics/MakeTest/hello.go
@@ -5,13 +5,14 @@ import (
 )
 
 func main() {
-	totalWins := map[string]int{}
-	totalWins["Orcas"] = 1
-	totalWins["Lions"] = 2
-	fmt.Println(totalWins["Orcas"])
-	fmt.Println(totalWins["Kittens"])
-	totalWins["Kittens"]++
-	fmt.Println(totalWins["Kittens"])
-	totalWins["Lions"] = 3
-	fmt.Println(totalWins)
+	m := map[string]int{
+		"hello": 5,
+		"world": 0,
+	}
+	v, ok := m["hello"]
+	fmt.Println(v, ok)
+	v, ok = m["world"]
+	fmt.Println(v, ok)
+	v, ok = m["goodbye"]
+	fmt.Println(v, ok)
 }
